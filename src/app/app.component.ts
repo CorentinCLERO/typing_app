793a97b41ad5974fde23a73934992e8c8cb08b7d
CorentinCLERO <corentinclero@hotmail.com>
2025-03-09 17:12:42 +0100

    add text follow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'typing_app';
+  phrase: string[] = 'Je vais'.split('');
+  key: string = '';
+  currentIndex: number = 0;
+  typedCharacters: { char: string, isCorrect: boolean }[] = [];
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    event.preventDefault()
+    if (this.currentIndex < this.phrase.length) {
+      if (!["Shift", "AltGraph", "Control", "Alt"].includes(event.key)) {
+        this.key = event.key;
+        const isCorrect = this.key === this.phrase[this.currentIndex];
+        
+        // Stocker le caractère tapé et sa validité
+        this.typedCharacters[this.currentIndex] = {
+          char: this.key,
+          isCorrect: isCorrect
+        };
+  
+        // Avancer à la prochaine lettre seulement si la frappe est correcte
+        if (isCorrect) {
+          if (this.currentIndex === this.phrase.length - 1) {
+            this.currentIndex = 0
+            this.typedCharacters = []
+            this.key = ''
+          } else {
+            this.currentIndex++;
+          }
+        }
+      }
+    }
+  }
 }
